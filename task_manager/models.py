72bd602d8b2fd08299c2c7c9e74e169f4ba151e9
AndreiVed <catblues87@gmail.com>
2024-09-01 20:00:13 +0300

    create models to worker, position, task, tasktype, project, team

--- task_manager/models.py
+++ task_manager/models.py
@@ -1,3 +1,96 @@
-from django.db import models
 
-# Create your models here.
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+from django.urls import reverse
+
+
+class Position(models.Model):
+    name = models.CharField(max_length=68, unique=True)
+
+    class Meta:
+        ordering = ["name"]
+
+    def __str__(self):
+        return self.name
+
+
+class Project(models.Model):
+    name = models.CharField(max_length=68, unique=True)
+
+    class Meta:
+        ordering = ["name"]
+
+    def __str__(self):
+        return self.name
+
+
+class Team(models.Model):
+    name = models.CharField(max_length=68, unique=True)
+    projects = models.ManyToManyField(Project, related_name="teams")
+
+    class Meta:
+        ordering = ["name"]
+
+    def __str__(self):
+        return self.name
+
+
+class Worker(AbstractUser):
+    position = models.ForeignKey(Position, on_delete=models.CASCADE, related_name="workers")
+    team = models.ForeignKey(Team, on_delete=models.CASCADE, related_name="workers")
+
+    class Meta:
+        verbose_name = "worker"
+        verbose_name_plural = "workers"
+
+    def __str__(self):
+        return f"{self.username} ({self.first_name} {self.last_name}) - position: {self.position}"
+
+    def get_absolute_url(self):
+        return reverse("task_manager:worker-detail", kwargs={"pk": self.pk})
+
+
+class TaskType(models.Model):
+    name = models.CharField(max_length=68, unique=True)
+
+    class Meta:
+        ordering = ["name"]
+
+    def __str__(self):
+        return self.name
+
+
+class Task(models.Model):
+    HIGH = 'high'
+    MEDIUM = 'medium'
+    LOW = 'low'
+
+    PRIORITY_CHOICES = [
+        (HIGH, 'High Priority'),
+        (MEDIUM, 'Medium Priority'),
+        (LOW, 'Low Priority'),
+    ]
+
+    name = models.CharField(max_length=68, unique=True)
+    description = models.TextField()
+    created_date = models.DateTimeField(auto_now=True)
+    deadline = models.DateTimeField()
+    priority = models.CharField(
+        max_length=68,
+        choices=PRIORITY_CHOICES,
+        default=MEDIUM
+    )
+    task_type = models.ForeignKey(
+        TaskType,
+        on_delete=models.CASCADE,
+        related_name="tasks"
+    )
+    assignees = models.ManyToManyField(Worker, related_name="tasks")
+    project = models.ForeignKey(
+        Project,
+        on_delete=models.CASCADE,
+        related_name="tasks"
+    )
+
+    class Meta:
+        ordering = ["-deadline"]
