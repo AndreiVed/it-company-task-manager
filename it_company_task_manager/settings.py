9ff85f3285d478ef1b68c37b338973d14af537c5
AndreiVed <catblues87@gmail.com>
2024-09-27 00:03:03 +0300

    change settings secret_key, create .env and modify

--- it_company_task_manager/settings.py
+++ it_company_task_manager/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 import os
 from pathlib import Path
+from decouple import config
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -19,8 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ("django-insecure-*0xa"
-              "(xm$+rz_ph%=jj_3q)xki$k8ssz8_d%4h(szglvtxt^15=")
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
